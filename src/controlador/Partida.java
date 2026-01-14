486f6618d2241f4ef8262ea0a862d3941d703a81
Ment0lin <fernando.escribano@juanxxiii.net>
2026-01-14 10:08:29 +0100

    Fer. Clase Partida

---
+++ src/controlador/Partida.java
@@ -0,0 +1,39 @@
+package controlador;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+import modelo.Diccionario;
+import modelo.Jugador;
+import vista.Consola;
+
+public class Partida {
+    private ArrayList<Jugador> jugadores = new ArrayList<>();
+    private Consola consola = new Consola();
+    private Diccionario dict = new Diccionario();
+    private Scanner teclado  = new Scanner(System.in);
+
+    public void registrarJugadores() {
+        int cantidad = consola.pideInt("¿Cuántos jugadores sois? Introduzca un número: ");
+        //teclado.nextLine(); //---> AQUÍ NO FUNCIONA
+        for (int i = 0; i < cantidad; i++) {
+            String nombre = consola.pideString("Nombre del jugador " + (i + 1));
+            jugadores.add(new Jugador(nombre));
+        }
+    }
+
+    public void jugarRonda() { //en cada ronda, se mantienen los jugadores, pero no los roles
+        String palabraSecreta = dict.obtenerPalabraAleatoria();      
+        int nImpostores = consola.pideInt("¿Cuántos impostores habrá? Introduzca un número: ");
+        Collections.shuffle(jugadores); //utilizamos este método para barajear
+
+        //El primer jugador va a ser el árbitro
+        jugadores.get(0).setRol("ARBITRO");
+
+        //los siguientes N son los impostores
+        for (int i = 1; i <= nImpostores; i++) {
+            jugadores.get(i).setRol("IMPOSTOR");
+        }
+        consola.mostrarResultados(jugadores, palabraSecreta);
+    }  
+}
--- src/vista/Consola.java
+++ src/vista/Consola.java
@@ -1,13 +1,46 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package vista;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import modelo.Jugador;
 
 /**
  *
- * @author Lazaro
+ * @author claudiaescuderomontoro
  */
 public class Consola {
+    private Scanner teclado  = new Scanner(System.in);
     
+    public String pideString(String mensaje){
+        System.out.print(mensaje + ": ");
+        return teclado.nextLine();
+    }
+    
+    public int pideInt(String msg) {
+        System.out.print(msg);
+        int n = teclado.nextInt();
+        teclado.nextLine(); // buffer
+        return n;
+    }
+
+    public void mostrar(String mensaje){
+        System.out.println(mensaje);
+    }
+
+    public void mostrarResultados(ArrayList<Jugador> lista, String palabra) {
+        System.out.printf("\n\t %c ROLES ASIGNADOS %c\n", 126, 126);
+        for (Jugador j : lista){
+            if(j.getRol().equals("ARBITRO")){
+                mostrar(j.getNombre() + " - ARBITRO");
+                mostrar("Pulsa intro, si eres el árbitro, para empezar a jugar...");
+                teclado.nextLine();
+            }
+            else if (j.getRol().equals("IMPOSTOR")){
+                mostrar(j.getNombre() + " - IMPOSTOR");
+            } 
+        }
+        
+        mostrar("\nPALABRA: " + palabra);
+        mostrar("");
+    }
 }
